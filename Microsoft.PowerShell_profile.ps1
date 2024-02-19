8385c11fe869b278ec0cd57a2993b2a582d4736e
Martin andersen <martinandersen3d@gmail.com>
2024-02-20 00:11:24 +0100

    nav to dir

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -58,6 +58,52 @@ function s {
     }
 }
 
+# Navigate directorys 
+function g {
+    $userDir = [System.Environment]::ExpandEnvironmentVariables($env:UserProfile)
+    $dirs = @(
+        "C:\",
+        "C:\MartinPrivat",
+        "C:\temp",
+        "$userDir",
+        "$userDir\source\repos",
+        "$userDir\Documents",
+        "$userDir\Downloads",
+        "$userDir\Desktop",
+        "$userDir\Videos",
+        "$userDir\.vscode",
+        "$userDir\AppData\Local\Microsoft\VisualStudio",
+        "$userDir\Obsidian2",
+        "$userDir\Desktop\SCREENSHOTS",
+        "$userDir\Documents\WindowsPowerShell"
+        "C:\Program Files",
+        "C:\Program Files (x86)",
+        "C:\ProgramData"
+    )
+
+    # $args is an automatic variable that contains any method arguments
+    if ($args) {
+        # Method parameter is set
+
+        # $args[0] is the first argument (i.e. the index of the directory to navigate to)
+        $param = $args[0]
+
+        # Get array item by index
+        $arrayItem = $dirs[$param-1]
+
+        # Use the Set-Location cmdlet (or its alias cd) to navigate to the directory
+        Set-Location $arrayItem
+    }
+    else {
+        # No method parameters send, will print a list of directories
+        Write-Host ""
+        for ($i = 0; $i -lt $dirs.Length; $i++) {
+            Write-Host "$($i + 1) $($dirs[$i])"
+        }
+        Write-Host ""
+    }
+}
+
 # If so and the current host is a command line, then change to red color 
 # as warning to user that they are operating in an elevated context
 # Useful shortcuts for traversing directories
