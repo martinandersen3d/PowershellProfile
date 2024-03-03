42b636c123704fa964978479995a78a6134548f1
Martin andersen <martinandersen3d@gmail.com>
2024-03-03 19:42:15 +0100

    fix2

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -28,7 +28,7 @@ function clr { Clear-Host }
 function clear { Clear-Host }
 
 # Update scripts from the git repo
-function u { Invoke-RestMethod "https://github.com/martinandersen3d/PowershellProfile/raw/main/setup.ps1" | Invoke-Expression }
+function u { irm "https://github.com/martinandersen3d/PowershellProfile/raw/main/setup.ps1" | iex }
 
 
 # Fzf Subdirs 3 leves down, that does not start with  ".git" or is "node_modules"
