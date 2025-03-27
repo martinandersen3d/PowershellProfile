5b5f3fbe19f04b2930dd9c02b2f379a6d073dcb7
Martin andersen <martinandersen3d@gmail.com>
2025-03-27 12:25:42 +0100

    remove winget autocomplete

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -705,13 +705,13 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 }
 
 # WinGet command Autocomplete with TAB Key -----------------------------------------------------------------
-https://github.com/microsoft/winget-cli/blob/1fbfacc13950de8a17875d40a8beb99fc6ada6c2/doc/Completion.md
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $Local:word = $wordToComplete.Replace('"', '""')
-        $Local:ast = $commandAst.ToString().Replace('"', '""')
-        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-}
+# https://github.com/microsoft/winget-cli/blob/1fbfacc13950de8a17875d40a8beb99fc6ada6c2/doc/Completion.md
+# Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+#     param($wordToComplete, $commandAst, $cursorPosition)
+#         [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+#         $Local:word = $wordToComplete.Replace('"', '""')
+#         $Local:ast = $commandAst.ToString().Replace('"', '""')
+#         winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+#             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+#         }
+# }
