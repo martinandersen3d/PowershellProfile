de25747092796e17056abd706e1b53e94e49e5ec
Martin andersen <martinandersen3d@gmail.com>
2025-04-03 00:48:28 +0200

    --no-line-numbers

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -383,7 +383,7 @@ function ll { Get-ChildItem -Path $pwd -File }
 function GitCheatsheet {
     $filePath = "$HOME\Documents\WindowsPowerShell\git-cheatsheet.md"
     if (Test-Path $filePath) {
-        bat --paging=never $filePath
+        bat --paging=never --no-line-numbers $filePath
     } else {
         Write-Host "Cheatsheet not found: $filePath" -ForegroundColor Red
     }
