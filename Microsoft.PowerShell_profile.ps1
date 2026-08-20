8d2f572627b93178a90cd25d1ddc1f83c52b13b6
Martin andersen <martinandersen3d@gmail.com>
2026-08-20 14:10:53 +0200

    remove color

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1539,7 +1539,7 @@ if ($PSVersionTable.PSVersion.Major -ge 7) {
 
         # Carapace terminal completions setup
         $env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense' # optional
-        Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+        # Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
         Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
         carapace _carapace | Out-String | Invoke-Expression
 
